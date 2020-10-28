08d1bfa22a4cfd82e7fc37da059a10f4ded4a83f
Jack Muskopf <jmuskopf@wustl.edu>
2020-10-28 13:41:40 -0500

    birds 2.0

--- birds.py
+++ birds.py
@@ -2,13 +2,19 @@ import random as r
 import numpy as np
 import turtle as birds
 
+flock_start_speed = 300
+flock_theta_variance = .3
+flock_speed_variance = 10
+bird_error_variance = 60
+n_birds = 30
+
 def is_even(n):
     if n%2 == 0:
         return True
     else:
         return False
 
-class MyBird(object):
+class Bird(object):
 
     def __init__(self,number,flock):
         self.birdie = birds.Turtle()
@@ -25,17 +31,24 @@ class MyBird(object):
         # set in flock.set_spots
         self.xspot = self.xspot_o
         self.yspot = self.yspot_o
+        self.x = self.xspot_o
+        self.y = self.yspot_o
+
+    def goto(self, x, y):
+        self.x = x
+        self.y = y
+        self.birdie.goto(x,y)
 
 
 
 class Flock(object):
 
-    def __init__(self,nbirds,theta=0,speed=300):
+    def __init__(self,nbirds,theta=0,speed=flock_start_speed):
         colors  = ["red","green","blue","orange","purple","pink","yellow"]
         self.nbirds = nbirds
         self.offset = nbirds/2 + 1
-        self.dummies = [MyBird(j,self) for j in range(nbirds)]
-        for bird in self.dummies:
+        self.birdies = [Bird(j,self) for j in range(nbirds)]
+        for bird in self.birdies:
             bird.birdie.color(r.choice(colors))
             bird.birdie.shape('turtle')
         self.theta = theta
@@ -44,12 +57,17 @@ class Flock(object):
         self.y = 0
         self.xcoords = None
         self.ycoords = None
+
+        self.past_x = list()
+        self.past_y = list()
         self.reset_coords()
 
     def reset_coords(self):
-        positions = [bird.birdie.position() for bird in self.dummies]
+        positions = [bird.birdie.position() for bird in self.birdies]
         self.xcoords = [p[0] for p in positions]
         self.ycoords = [p[1] for p in positions]
+        self.past_x.extend(self.xcoords)
+        self.past_y.extend(self.ycoords)
         self.x = sum(self.xcoords)/float(self.nbirds)
         self.y = sum(self.ycoords)/float(self.nbirds)
 
@@ -59,102 +77,60 @@ class Flock(object):
     def set_spots(self):
 
         # get birds into V formation
-        for bird in self.dummies:
+        for bird in self.birdies:
             # rotation matrix
             bird.xspot = np.cos(self.theta)*bird.xspot_o - np.sin(self.theta)*bird.yspot_o
             bird.yspot = np.sin(self.theta)*bird.xspot_o + np.cos(self.theta)*bird.yspot_o
 
 
 
-    def move(self,speed=None,theta=None):
-        if speed is None:
-            speed = abs(np.random.normal(1,1))
-            self.speed = speed
-        if theta is None:
-            theta = self.theta - np.random.normal(0,.0015)
-            self.theta = theta
+    def move(self,dv=None,dtheta=None):
+        if dv is None:
+            dv = np.random.normal(0,flock_speed_variance)
+        self.speed += dv
+        if dtheta is None:
+            dtheta = np.random.normal(0,flock_theta_variance)
+        self.theta += dtheta
 
         self.set_spots()
 
-        flock_xv = speed*np.cos(theta)
-        flock_yv = speed*np.sin(theta)
+        flock_xv = self.speed*np.cos(self.theta)
+        flock_yv = self.speed*np.sin(self.theta)
 
         positions = []
-        for bird in self.dummies:
-            bx, by = bird.birdie.position()
-            to_spot = ((self.x+bird.xspot)/2.0-bx,(self.y+bird.yspot)/2.0-by)
-            dbx = to_spot[0]+flock_xv + np.random.normal(30,10)
-            dby = to_spot[1]+flock_yv + np.random.normal(30,10)
-            bird.birdie.goto(bx+dbx,by+dby)
+        for bird in self.birdies:
+            dbx = flock_xv + np.random.normal(0,bird_error_variance)
+            dby = flock_yv + np.random.normal(0,bird_error_variance)
+            print(dbx, dby)
+            print(bird.x, bird.y)
+            bird.goto(bird.x+dbx,bird.y+dby)
 
 
         self.reset_coords()
-
-        # for bird in self.dummies:
-        #     shapeshize = 500/float()
-        #     bird.shapesize
-
-
-            # btheta = theta - np.random.normal(0,10)
-            # bspeed = np.random.normal(speed,15)
-            # bird.right(btheta)
-            # bird.forward(bspeed)
 
 
 wn = birds.Screen()
 wn.bgcolor("lightgreen")
 wn.setworldcoordinates(-500,-500,500,500)
 
-myFlock = Flock(10)
-mx = max(myFlock.xcoords)
-ix = min(myFlock.xcoords)
-my = max(myFlock.ycoords)
-iy = min(myFlock.ycoords)
+flock = Flock(n_birds)
+mx = max(flock.xcoords)
+ix = min(flock.xcoords)
+my = max(flock.ycoords)
+iy = min(flock.ycoords)
 
 
-xs = [myFlock.x]
-ys = [myFlock.y]
+xs = [flock.x]
+ys = [flock.y]
 
 while True:
 
-    myFlock.move()
-    xs.append(myFlock.x)
-    ys.append(myFlock.y)
-    mx = max(xs)
-    ix = min(xs)
-    my = max(ys)
-    iy = min(ys)
-    d = 300
+    flock.move()
+    xs.append(flock.x)
+    ys.append(flock.y)
+    mx = max(flock.past_x)
+    ix = min(flock.past_x)
+    my = max(flock.past_y)
+    iy = min(flock.past_y)
+    d = 3*abs(flock.speed)
     wn.setworldcoordinates(ix-d,iy-d,mx+d,my+d)
-
-    # d = max([abs(mx-ix),abs(my-iy)])+10
-    # wn.setworldcoordinates(-1*d+myFlock.x,-1*d+myFlock.y,d+myFlock.x,d+myFlock.y)
-
-
-    #zoomin out
-    # myFlock.move()
-    # mx = max(myFlock.xcoords+[mx])
-    # ix = min(myFlock.xcoords+[ix])
-    # my = max([my]+myFlock.ycoords)
-    # iy = min([iy]+myFlock.ycoords)
-    #
-    # d = max([abs(mx-ix),abs(my-iy)])+100
-    # wn.setworldcoordinates(-1*d,-1*d,d,d)
-
-    #stay on em
-    # mx = myFlock.x+500
-    # ix = myFlock.x-500
-    # my = myFlock.y + 500
-    # im = myFlock.y - 500
-    # wn.setworldcoordinates(ix,im,mx,my)
-
-    # mx = max(myFlock.xcoords)
-    # ix = min(myFlock.xcoords)
-    # my = max(myFlock.ycoords)
-    # im = min(myFlock.ycoords)
-    # d = max([mx,my,abs(im),abs(ix)]) + 500
-    # wn.setworldcoordinates(-1*d,-1*d,d,d)
-    # dmax = max([mx,my,dmax])+50
-    # dmin = min([ix,im,dmin])-50
-    # d = max([abs(dmin),dmax])
-    # wn.setworldcoordinates(-1*d,-1*d,d,d)
